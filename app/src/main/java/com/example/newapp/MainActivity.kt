3e814627e09363349c741a3d18b492e8ef808235
admin <urmasuper93@gmail.com>
2023-02-10 19:13:38 +0600

    new 2

--- app/src/main/java/com/example/newapp/MainActivity.kt
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -14,16 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textViewDiplay = findViewById(R.id.buttonPlus)
+        textViewDiplay = findViewById(R.id.textViewDisplay)
+        buttonPlus = findViewById(R.id.buttonPlus)
+
 
     }
 
 
+    fun textAdd(view: View) {
+        textViewDiplay.text = textViewDiplay.text.toString() + buttonPlus.text.toString()
 
-    fun textAdd(view: View)
+    }
+
+    fun clearAll(view: View) {
+        textViewDiplay.text = "0"
+    }
+
+    fun backspace(view: View)
     {
 
+        val temp  = textViewDiplay.text.chunked(1)
+
+        var z = ""
+        var i = 0
+        while (i<temp.size-1)
+        {
+            z+=temp[i]
+            i++
+        }
+
+        textViewDiplay.text = z
+
+
+
+
+
     }
+
 
 
 }
